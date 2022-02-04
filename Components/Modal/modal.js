cb095b535f556cff442dfd413dfd11690376c924
Nitin kalra <nikkalra88@gmail.com>
2022-02-04 20:00:34 +0530

    refactor: CSS
    imported all css files in one and now using it for all the component. Pushing it directly as there are no new feat or bugs.

--- Components/Modal/modal.js
+++ Components/Modal/modal.js
@@ -1,21 +1,21 @@
 var btn = document.getElementById("btn");
 var closes = document.getElementById("closes");
 var modal = document.getElementById("modal-content");
-var wrapper = document.getElementById("wrapper");
+var wrapper = document.getElementById("modal-wrapper");
 
 btn.onclick = () => {
   modal.style.display = "flex";
-  wrapper.classList.add("wrapper");
+  wrapper.classList.add("modal-wrapper");
 };
 
 closes.onclick = () => {
   modal.style.display = "none";
-  wrapper.classList.remove("wrapper");
+  wrapper.classList.remove("modal-wrapper");
 };
 
 window.onclick = (event) => {
   if (event.target == wrapper) {
     modal.style.display = "none";
-    wrapper.classList.remove("wrapper");
+    wrapper.classList.remove("modal-wrapper");
   }
 };
