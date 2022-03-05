d9e1cc25f1cdb848a87bda813f03238eeda055a2
Nitin kalra <nikkalra88@gmail.com>
2022-03-05 23:09:30 +0530

    fix: navbar

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -1,16 +1,11 @@
 const nav = document.getElementById("nav");
 const open_nav = document.getElementById("open-nav");
-const close_nav = document.getElementById("close-nav");
-
-// close_nav.onclick = () => (nav.style.width = "0");
 
 open_nav.onclick = () => {
   if (nav.style.width === "0px" || nav.style.width == 0) {
     nav.style.width = "15rem";
-    console.log(nav.style.width);
   } else {
     nav.style.width = "0px";
-    console.log(nav.style.width);
   }
 };
 
